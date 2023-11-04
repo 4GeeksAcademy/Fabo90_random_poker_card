9cf53901c9be1481a3e1233bf7271c5d9c4f6cee
Fabo90 <139248863+Fabo90@users.noreply.github.com>
2023-11-04 06:50:45 +0000

    FabianCard

--- src/app.js
+++ src/app.js
@@ -1,11 +1,48 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector(".number").innerHTML = randomNumber();
+  document.querySelector(".top").innerHTML = twoIcons;
+  document.querySelector(".bottom").innerHTML = twoIcons;
+  changeColorTop();
+  changeColorBottom();
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let randomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let index = Math.floor(Math.random() * numbers.length);
+  return numbers[index];
+};
+
+let randomIcon = () => {
+  let icons = ["♦", "♥", "♠", "♣"];
+  let index = Math.floor(Math.random() * icons.length);
+  return icons[index];
+};
+let twoIcons = randomIcon();
+
+let changeColorTop = () => {
+  if (twoIcons === "♦" || twoIcons === "♥") {
+    return (document.querySelector(".top").style.color = "red");
+  }
+};
+
+let changeColorBottom = () => {
+  if (twoIcons === "♦" || twoIcons === "♥") {
+    return (document.querySelector(".bottom").style.color = "red");
+  }
 };

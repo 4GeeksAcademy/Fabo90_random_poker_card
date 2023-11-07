ee48feb385609524ef98933d1afb1c1487e4395e
Fabo90 <139248863+Fabo90@users.noreply.github.com>
2023-11-07 15:09:10 +0000

    finalCard

--- src/app.js
+++ src/app.js
@@ -1,48 +1,56 @@
 import "./style.css";
 
 window.onload = () => {
-  document.querySelector(".number").innerHTML = randomNumber();
-  document.querySelector(".top").innerHTML = twoIcons;
-  document.querySelector(".bottom").innerHTML = twoIcons;
-  changeColorTop();
-  changeColorBottom();
-};
-
-let randomNumber = () => {
-  let numbers = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-  let index = Math.floor(Math.random() * numbers.length);
-  return numbers[index];
-};
-
-let randomIcon = () => {
-  let icons = ["♦", "♥", "♠", "♣"];
-  let index = Math.floor(Math.random() * icons.length);
-  return icons[index];
-};
-let twoIcons = randomIcon();
-
-let changeColorTop = () => {
-  if (twoIcons === "♦" || twoIcons === "♥") {
-    return (document.querySelector(".top").style.color = "red");
-  }
-};
-
-let changeColorBottom = () => {
-  if (twoIcons === "♦" || twoIcons === "♥") {
-    return (document.querySelector(".bottom").style.color = "red");
-  }
+  const number = document.querySelector("#number");
+  const topSuit = document.querySelector("#top");
+  const bottomSuit = document.querySelector("#bottom");
+  const CardBtn = document.querySelector("#button1");
+  let randomNumber = () => {
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let index = Math.floor(Math.random() * numbers.length);
+    return numbers[index];
+  };
+  let randomIcon = () => {
+    let icons = ["♦", "♥", "♠", "♣"];
+    let index = Math.floor(Math.random() * icons.length);
+    if (index === 0 || index === 1) {
+      topSuit.style.color = "red";
+      bottomSuit.style.color = "red";
+      return icons[index];
+    } else if (index === 2 || index === 3) {
+      topSuit.style.color = "black";
+      bottomSuit.style.color = "black";
+      return icons[index];
+    }
+  };
+  let twoIcons = randomIcon();
+  number.innerHTML = randomNumber();
+  topSuit.innerHTML = twoIcons;
+  bottomSuit.innerHTML = twoIcons;
+  CardBtn.addEventListener("click", () => {
+    twoIcons = randomIcon();
+    number.innerHTML = randomNumber();
+    topSuit.innerHTML = twoIcons;
+    bottomSuit.innerHTML = twoIcons;
+  });
+  setInterval(() => {
+    twoIcons = randomIcon();
+    number.innerHTML = randomNumber();
+    topSuit.innerHTML = twoIcons;
+    bottomSuit.innerHTML = twoIcons;
+  }, 4000);
 };
